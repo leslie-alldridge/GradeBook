6d06fc422866603adeb76e0d29eabbf25ebd9aeb
leslie-alldridge <leslie.alldridge@gmail.com>
2020-11-17 15:43:02 +1300

    Calc total score

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,7 +6,21 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = new[] { 12.7, 2.4, 8.9 };
+            var result = 0.0;
+            foreach (double num in numbers)
+            {
+                result += num;
+            }
+            Console.WriteLine($"The final result is {result}");
+
+            if (args.Length > 0){
+                Console.WriteLine($"Hello {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
         }
     }
 }
