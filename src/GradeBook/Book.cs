e989446239eab05529c744aaeda248f8818d5dab
leslie-alldridge <leslie.alldridge@gmail.com>
2020-11-21 09:26:43 +1300

    Encapsulate logic

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,5 @@
+using System;
+using static System.Console;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -6,15 +8,35 @@ namespace GradeBook
     {
         private List<double> grades;
         private string name;
+        private double result = 0.00;
+        private double highGrade = double.MinValue;
+        private double lowGrade = double.MaxValue;
+        private double average;
 
-        public Book (string name)
+        public Book(string name)
         {
             grades = new List<double>();
             this.name = name;
         }
-       public void AddGrade(double grade)
+        public void AddGrade(double grade)
         {
             grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            foreach (var grade in grades)
+            {
+                highGrade = Math.Max(grade, highGrade);
+                lowGrade = Math.Min(grade, lowGrade);
+                result += grade;
+            }
+            average = result / grades.Count;
+
+            WriteLine($"Statistics for Book: {name}");
+            WriteLine($"Average: {average}");
+            WriteLine($"Min score: {lowGrade}");
+            WriteLine($"Max score: {highGrade}");
         }
 
     }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace GradeBook
+﻿namespace GradeBook
 {
     class Program
     {
@@ -11,24 +8,7 @@ namespace GradeBook
             book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(77.5);
-
-            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
-            grades.Add(56.1);
-
-            var result = 0.0;
-
-            var highGrade = double.MinValue;
-            var lowGrade = double.MaxValue;
-
-            foreach (var grade in grades)
-            {
-                highGrade = Math.Max(grade, highGrade);
-                lowGrade = Math.Min(grade, lowGrade);
-                result += grade;
-            }
-            var average = result / grades.Count;
-            
-            Console.WriteLine($"The final result is: {result:N1} with an average of {average:N1}, high of {highGrade:N1} and a low of {lowGrade:N1}");
+            book.ShowStatistics();
         }
     }
 }
