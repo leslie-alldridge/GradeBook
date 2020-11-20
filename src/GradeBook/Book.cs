7e02dc2f6d07e68a0f0ee0a139746744e7c6f3dc
leslie-alldridge <leslie.alldridge@gmail.com>
2020-11-21 09:20:00 +1300

    High, low and average grade

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -4,10 +4,18 @@ namespace GradeBook
 {
     class Book
     {
-        List<double> grades;
+        private List<double> grades;
+        private string name;
+
+        public Book (string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
        public void AddGrade(double grade)
         {
             grades.Add(grade);
         }
+
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,18 +7,28 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var book = new Book();
+            var book = new Book("Leslie's Book");
             book.AddGrade(89.1);
-            var grades = new List<double>() { 12.7, 2.4, 8.9 };
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
-            var result = 0.00;
+
+            var result = 0.0;
+
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
             foreach (var grade in grades)
             {
+                highGrade = Math.Max(grade, highGrade);
+                lowGrade = Math.Min(grade, lowGrade);
                 result += grade;
             }
             var average = result / grades.Count;
             
-            Console.WriteLine($"The final result is: {result:N1} with an average of {average:N1}");
+            Console.WriteLine($"The final result is: {result:N1} with an average of {average:N1}, high of {highGrade:N1} and a low of {lowGrade:N1}");
         }
     }
 }
